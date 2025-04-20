bd1bca6c5690d56ddee079ea4bc68821dacba1c9
edy alves <30.edyalves@gmail.com>
2025-04-20 18:31:08 -0300

    Creat profile view controller.

---
+++ ChatApp/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ProfileViewController.swift
+//  ChatApp
+//
+//  Created by ednardo alves on 17/04/25.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func logout() {
+        do {
+            try Auth.auth().signOut()
+            let authStoryboard = UIStoryboard(name: "Auth", bundle: nil)
+            let signinVC = authStoryboard.instantiateViewController(withIdentifier: "SignInViewController")
+            let window = UIApplication.shared.connectedScenes.flatMap { ($0 as? UIWindowScene)?.windows ?? []}.first {$0.isKeyWindow}
+            window?.rootViewController = signinVC
+        } catch {
+            presentErroAlert(title: "Logout failed", message: "Something went wrong with logout. Please try again later.")
+        }
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+        let logoutAlert = UIAlertController(title: "Confirm Logout", message: "Are you sure you would like to logout of your account?", preferredStyle: .alert)
+        let logoutAction = UIAlertAction(title: "Logout", style: .destructive) { _ in
+            self.logout()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        logoutAlert.addAction(logoutAction)
+        logoutAlert.addAction(cancelAction)
+        present(logoutAlert, animated: true)
+    }
+    
+}
